f00a90b76b2587586aac8bab184077f2f86e7678
technicalace <arguelloandrew15@gmail.com>
2024-11-07 13:30:13 -0600

    first commit

---
+++ stringstringMap.cpp
@@ -0,0 +1,23 @@
+/*
+
+String String Map 
+
+11/02/24
+
+@AJ Enrique Arguello
+
+Objectives: Create a string string Map that will connect Relationship ("Family", "Co-Worker", "Friend") to specific People ("names")
+
+  Add a name to each category
+
+  Display categories and names
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+}
