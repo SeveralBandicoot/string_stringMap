4ec1e1b74964ee2e563d063e7d2c914c7d6d5b1b
technicalace <arguelloandrew15@gmail.com>
2024-11-11 19:10:16 -0600

    Final draft

--- stringstringMap.cpp
+++ stringstringMap.cpp
@@ -15,9 +15,21 @@ Objectives: Create a string string Map that will connect Relationship ("Family",
 */
 
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main() {
-    
+    std::map<std::string, std::string> relationshipPeople; // string string map initialization
+    // map contents relationship->name
+    relationshipPeople["Family"] = "John Scott";
+    relationshipPeople["Co-Worker"] = "Mike Handson";
+    relationshipPeople["Friend"] = "Alex Wock";
+    // auto print, prints both relationship/name
+    for (auto print:relationshipPeople) {
+      cout << "Relationship: " << print.first << " || Name: " << print.second << endl;
+    }
+
+    return 0;
 }
